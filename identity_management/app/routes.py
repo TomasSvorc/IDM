1f59ea4b796231f890e22d323649475b8c1398d4
TomasSvorc <74916620+TomasSvorc@users.noreply.github.com>
2024-06-05 15:02:48 +0200

    v1

---
+++ identity_management/app/routes.py
@@ -0,0 +1,79 @@
+from flask import render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from app import app, db, login_manager
+from app.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('admin'))
+        flash('Invalid credentials')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/admin')
+@login_required
+def admin():
+    users = User.query.all()
+    return render_template('admin.html', users=users)
+
+@app.route('/add_user', methods=['POST'])
+@login_required
+def add_user():
+    username = request.form['username']
+    password = request.form['password']
+    hashed_password = generate_password_hash(password, method='sha256')
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('admin'))
+
+@app.route('/edit_user/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_user(id):
+    user = User.query.get(id)
+    if request.method == 'POST':
+        user.username = request.form['username']
+        user.password = generate_password_hash(request.form['password'], method='sha256')
+        db.session.commit()
+        return redirect(url_for('admin'))
+    return render_template('edit_user.html', user=user)
+
+@app.route('/delete_user/<int:id>', methods=['POST'])
+@login_required
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('admin'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
