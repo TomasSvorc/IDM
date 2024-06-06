151f8c95a1ff5f52a98f146f282d36b559c5fa81
TomasSvorc <74916620+TomasSvorc@users.noreply.github.com>
2024-06-06 17:00:58 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import app, db
 
-with app.app_context():
-    db.create_all()
+def create_app():
+    with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
     db.create_all()
